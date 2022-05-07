2f9a10616c2e03ff498b10107c1b6e0617b3d5f1
«Alex» <aser_e@mail.ru>
2022-05-07 20:54:42 +0900

    zadacha2 corrected

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -14,53 +14,41 @@ void printArray(int[,] arr)
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
-            Console.Write(arr[i, j] + " ");
+            Console.Write(String.Format("{0,4}", arr[i, j]));
         Console.WriteLine();
     }
 
 }
-int[,] changeArray(int[,] arr, int[,] arr1)
+void multiplyMatrices(int[,] arr1, int[,] arr2)
 {
-
-    int[,] arr2 = new int[arr.GetLength(0), arr.GetLength(1)];
-    for (int i = 0; i < arr.GetLength(0); i++)
+    if (arr1.GetLength(1) == arr2.GetLength(0))
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
-            if (arr.GetLength(0) == arr1.GetLength(0) && arr.GetLength(1) == arr1.GetLength(1))
-                arr2[i, j] = arr[i, j] * arr1[i, j];
-    }
-    return arr2;
-    
-}
-bool replase(int[,] arr, int[,] arr1)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-            if (arr.GetLength(0) == arr1.GetLength(0) && arr.GetLength(1) == arr1.GetLength(1))
+        int[,] productArr = new int[arr1.GetLength(0), arr2.GetLength(1)];
+        for (int i = 0; i < arr1.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr2.GetLength(1); j++)
             {
-                return true;
+                for (int k = 0; k < arr2.GetLength(0); k++)
+                {
+                    productArr[i, j] += arr1[i, k] * arr2[k, j];
+                }
             }
+        }
+        printArray (productArr);
     }
-    return false;
+
+    else Console.WriteLine(" невозможно умножить матрицы ");
 }
 
-
+int a = 4, b = 3;
+int[,] array1 = new int[a, b];
 int m = 3, n = 3;
-int[,] array = new int[m, n];
-int k = 3, l = 3;
-int[,] array1 = new int[k, l];
-
-fillArray(array, 1, 3);
-fillArray(array1, 1, 3);
+int[,] array2 = new int[m, n];
+fillArray(array1, 1, 5);
+fillArray(array2, 1, 5);
 Console.WriteLine($"матрица1: ");
-printArray(array);
-Console.WriteLine();
-Console.WriteLine($"матрица2: ");
 printArray(array1);
-Console.WriteLine();
-int[,] result = changeArray(array, array1);
+Console.WriteLine($"матрица2: ");
+printArray(array2);
 Console.WriteLine($"Произведение двух матриц = ");
-if (replase(array,array1)) printArray(result);
-else Console.WriteLine(" невозможно умножить матрицы (матрица не квадратная) ");
-
+multiplyMatrices(array1, array2);
